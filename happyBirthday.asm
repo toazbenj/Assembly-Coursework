2ea20c768550ff9fcfb05f659a5feecdd4a9f307
Ben Toaz <90994176+toazbenj@users.noreply.github.com>
2023-10-03 20:28:02 -0400

    Create HappyBirthday.asm

---
+++ happyBirthday.asm
@@ -0,0 +1,73 @@
+		TTL Assembly Guide
+;***************************************************************
+; Initialization, loading and register work, happy birthday
+
+;***************************************************************
+;Assembler directives
+            THUMB
+;***************************************************************
+;EQUates
+;Vectors
+VECTOR_TABLE_SIZE EQU 0x000000C0  ;KL25Z
+VECTOR_SIZE       EQU 4           ;Bytes per vector
+;Memory
+SSTACK_SIZE EQU  0x00000100
+;NVIC
+ICER		EQU	 0xE000E180
+;**********************************************************************
+;Vector Table Mapped to Address 0 at Reset
+;Linker requires __Vectors to be exported
+            AREA    RESET, DATA, READONLY
+            EXPORT  __Vectors
+__Vectors 
+            DCD  SP_INIT        ;stack pointer value when stack is empty
+            DCD  Reset_Handler  ;reset vector
+			SPACE  (VECTOR_TABLE_SIZE - (2 * VECTOR_SIZE))
+            ALIGN
+;**********************************************************************
+            AREA    RAM,DATA,READWRITE
+;Allocate system stack
+SSTACK      SPACE   SSTACK_SIZE
+SP_INIT
+;Variables go here
+RAM_Data    SPACE   64    ;reserve space for 64 variables @ label Ram_Data
+
+;**********************************************************************
+;Program
+;Linker requires Reset_Handler
+            AREA    StudentCode,CODE,READONLY
+            ENTRY
+            EXPORT  Reset_Handler
+			
+Reset_Handler
+			CPSID   I       ;Mask interrupts
+; DISABLE INTERRUPTS
+			LDR 	R0, =ICER 	;R0 = &ICER
+			MOVS 	R1,	#0 		; R1 = 0
+			MVNS 	R1, R1 		; R1 = 0xFFFFFFFF
+			STR 	R1, [R0, #0]; ICER = 0xFFFFFFFF
+; INIT STACK POINTER
+			LDR R0, =SP_INIT 	; load base address
+			MOV SP, R0		; set SP to base address
+;**********************************************************************
+;**********************************************************************
+;**********************************************************************
+Main
+	LDR R0, BDAY
+	LDR R1, BMONTHs
+	LDR R2, BYEAR
+
+            B     	.       
+            ALIGN
+			
+;**********************************************************************
+;ROM Constants
+;**********************************************************************
+ROM_Data ;address label for constants stored in ROM
+
+BDAY DCD 0x8
+BMONTH DCD 0x9
+BYEAR DCD
+			
+			ALIGN 	; NEEDED AFTER CONSTANTS ARE DECLARED
+			END
